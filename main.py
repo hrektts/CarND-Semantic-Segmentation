bb754be35e76c6c5f9d29cfccb307e382746d86a
Katsutoshi Horie <mps299792458@gmail.com>
2017-11-05 10:41:03 +0900

    Delete initializer and regularizer

--- main.py
+++ main.py
@@ -45,25 +45,19 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     layer7_out = tf.layers.conv2d(vgg_layer7_out,
                                   num_classes,
                                   1,
-                                  padding='same',
-                                  kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
-                                  kernel_regularizer=tf.contrib.layers.l2_regularizer(2e-4))
+                                  padding='same')
     # upsampling
     layer4_in = tf.layers.conv2d_transpose(
         layer7_out,
         num_classes,
         4,
         strides=2,
-        padding='same',
-        kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
-        kernel_regularizer=tf.contrib.layers.l2_regularizer(2e-4))
+        padding='same')
     # 1x1 convolution
     layer4_in2 = tf.layers.conv2d(vgg_layer4_out,
                                   num_classes,
                                   1,
-                                  padding='same',
-                                  kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
-                                  kernel_regularizer=tf.contrib.layers.l2_regularizer(2e-4))
+                                  padding='same')
     # skip connection
     layer4_out = tf.add(layer4_in, layer4_in2)
     # upsampling
@@ -72,16 +66,12 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
         num_classes,
         4,
         strides=2,
-        padding='same',
-        kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
-        kernel_regularizer=tf.contrib.layers.l2_regularizer(2e-4))
+        padding='same')
     # 1x1 convolution
     layer3_in2 = tf.layers.conv2d(vgg_layer3_out,
                                   num_classes,
                                   1,
-                                  padding='same',
-                                  kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
-                                  kernel_regularizer=tf.contrib.layers.l2_regularizer(2e-4))
+                                  padding='same')
     # skip connection
     layer3_out = tf.add(layer3_in, layer3_in2)
     # upsampling
@@ -90,9 +80,7 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
         num_classes,
         16,
         strides=8,
-        padding='same',
-        kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
-        kernel_regularizer=tf.contrib.layers.l2_regularizer(2e-4))
+        padding='same')
 
     return output
 tests.test_layers(layers)
